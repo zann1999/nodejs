4d644811508bc2bd5c2119d678e9bc8df248e44b
Your Name <you@example.com>
2023-02-20 21:39:30 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// đây là phần import
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//http logger
+app.use(morgan("combined"));
+
+// template engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    // đổi đuuôi file
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+// set view folder
+app.set("views", path.join(__dirname, "resource/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
